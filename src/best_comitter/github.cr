43dd7166cadbad38d9c591f6f14c567c9e6aeaca
Pine Mizune <pinemz@gmail.com>
2015-11-14 15:32:05 +0900

    docs(github): add github API documen URL

--- src/best_comitter/github.cr
+++ src/best_comitter/github.cr
@@ -1,4 +1,5 @@
 module GitHub
+  # https://developer.github.com/v3/activity/events/types/
   module EventType
     ISSUES_EVENT       = "IssuesEvent"      # xiii
     PULL_REQUEST_EVENT = "PullRequestEvent" # xviii
